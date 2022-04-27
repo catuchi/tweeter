9386083981ce34fd12eb6d6bc3883fab5d0deb74
Chibu <atuchibueze@gmail.com>
2022-04-27 09:28:17 -0400

    creating the tweet section

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,5 +1,23 @@
 $(document).ready(function() {
   let counter = $('.counter');
+  let icon = $('.fa-solid.fa-flag');
+  let tweet = $('single-tweet');
+
+  icon.on('mouseover', function() {
+    $(this).css({ color: 'gold', transition: '100ms, ease-in-out'})
+  });
+
+  tweet.on('mouseout', function() {
+    $(this).css({ color: '#4056A1', transition: '100ms, ease-in-out'})
+  });
+  
+  tweet.on('mouseover', function() {
+    $('overlay').show();
+  });
+
+  icon.on('mouseout', function() {
+    $('overlay').hide();
+  });
 
   const characterCount = function(characters) {
     characterLimit = 140;
@@ -28,5 +46,7 @@ $(document).ready(function() {
 
     return countCheck(characterCheck);
   });
+
+  
 
 });
